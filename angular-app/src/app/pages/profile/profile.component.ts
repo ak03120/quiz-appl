70f2a209863774d8763a8c9a9a476b8a0181f730
ak03120 <ak03120@ymail.ne.jp>
2023-06-14 14:01:43 +0900

    プロフィール画面のモックアップを作成

--- angular-app/src/app/pages/profile/profile.component.ts
+++ angular-app/src/app/pages/profile/profile.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
-
+  name: string = '名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前名前';
+  correctCount: number = 50;
+  achievements: string[] = ["実績実績実績実績実績実績実績実績実績実績実績実績実績実績実績実績実績実績1", "実績2", "実績3"];
 }
