2001306f7163588dc37e79b6b2afb504e648933b
ak03120 <ak03120@ymail.ne.jp>
2023-08-15 00:01:11 +0900

    Strapiバックアップの更新

---
+++ strapi_old/dist/src/api/choice/routes/choice.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * choice router
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreRouter('api::choice.choice');
